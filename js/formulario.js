b3f97a964f38e630caf9e316aa3df1f4aeed8dc9
Kame1982 <inzunza.sanchez@gmail.com>
2022-02-17 23:15:39 -0300

    formulario reserva

--- js/formulario.js
+++ js/formulario.js
@@ -1,16 +1,157 @@
-document.getElementById("formulario.form").addEventListener("submit", (submitEvent) => {
-    submitEvent.preventDefault();
+//FORMULARIO NOMBRE--------------------------------------------------------------------
+document.getElementById("nombre").addEventListener("blur", (evento) => {
+    const input = evento.currentTarget;
+    const txtNombre = input.value;
+    
+    // let o const - formas recomendadas de variables o constantes
+    const feedbackNombre = document.getElementById("feedback-nombre");
 
-    const usuario     = document.getElementById("validationTooltip01").value;
-    const apellido    = document.getElementById("validationTooltip02").value;
-    const direccion   = document.getElementById("validationTooltip03").value;
-    const numero      = document.getElementById("validationTooltip04").value;
-    const telefono    = document.getElementById("validationTooltip05").value;
-    const correo      = document.getElementById("validationTooltip06").value;
-    const tapa        = document.getElementById("validationTooltip07").value;
+    if( txtNombre.length < 3 ) {        
+        feedbackNombre.innerHTML = "Favor de ingresar mas caracteres.";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        feedbackNombre.className = "invalid-feedback";
+    } else {        
+        feedbackNombre.innerHTML = "Listo";
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        feedbackNombre.className = "valid-feedback";
+    }
+});
 
-    if(usuario.length < 3 ){
-        comentario.ok();
-        
+document.getElementById("apellido").addEventListener("blur", (evento) => {
+    const input = evento.currentTarget;
+    const txtApellido = input.value;
+    
+    const feedbackApellido = document.getElementById("feedback-apellido");
 
-        function comentario.ok 
+    if( txtApellido.length < 3 ) {        
+        feedbackApellido.innerHTML = "Favor de ingresar mas caracteres.";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        feedbackApellido.className = "invalid-feedback";
+    } else {        
+        feedbackApellido.innerHTML = "Listo";
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        feedbackApellido.className = "valid-feedback";
+    }
+});
+
+document.getElementById("email").addEventListener("blur", (evento) => {
+    const input = evento.currentTarget;
+    const txtEmail = input.value;
+    
+    const feedbackEmail = document.getElementById("feedback-email");
+
+    if( txtEmail.length < 5 ) {        
+        feedbackEmail.innerHTML = "Favor de ingresar mas caracteres.";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        feedbackEmail.className = "invalid-feedback";
+    } else {        
+        feedbackEmail.innerHTML = "Listo";
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        feedbackEmail.className = "valid-feedback";
+    }
+});
+
+document.getElementById("telefono").addEventListener("blur", (evento) => {
+    const input = evento.currentTarget;
+    const txtTelefono = input.value;
+    
+    const feedbackTelefono = document.getElementById("feedback-telefono");
+
+    if( txtTelefono.length < 8 ) {        
+        feedbackTelefono.innerHTML = "Favor de ingresar un numero valido.";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        feedbackTelefono.className = "invalid-feedback";
+    } else {        
+        feedbackTelefono.innerHTML = "Listo";
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        feedbackTelefono.className = "valid-feedback";
+    }
+});
+
+document.getElementById("direccion").addEventListener("blur", (evento) => {
+    const input = evento.currentTarget;
+    const txtdireccion = input.value;
+    
+    // let o const - formas recomendadas de variables o constantes
+    const feedbackdireccion = document.getElementById("feedback-direccion");
+
+    if( txtdireccion.length < 3 ) {        
+        feedbackdireccion.innerHTML = "Favor de ingresar mas caracteres.";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        feedbackdireccion.className = "invalid-feedback";
+    } else {        
+        feedbackdireccion.innerHTML = "Listo";
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        feedbackdireccion.className = "valid-feedback";
+    }
+});
+
+document.getElementById("ncasa").addEventListener("blur", (evento) => {
+    const input = evento.currentTarget;
+    const txtncasa = input.value;
+    
+    // let o const - formas recomendadas de variables o constantes
+    const feedbackncasa = document.getElementById("feedback-ncasa");
+
+    if( txtncasa.length < 3 ) {        
+        feedbackncasa.innerHTML = "Más.";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        feedbackncasa.className = "invalid-feedback";
+    } else {        
+        feedbackncasa.innerHTML = "Listo";
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        feedbackncasa.className = "valid-feedback";
+    }
+});
+
+document.getElementById("provincia").addEventListener("blur", (evento) => {
+    const input = evento.currentTarget;
+    const txtprovincia = input.value;
+    
+    // let o const - formas recomendadas de variables o constantes
+    const feedbackprovincia = document.getElementById("feedback-provincia");
+
+    if( txtprovincia.length < 3 ) {        
+        feedbackprovincia.innerHTML = "no recuerdo esa provincia";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        feedbackprovincia.className = "invalid-feedback";
+    } else {        
+        feedbackprovincia.innerHTML = "Listo";
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        feedbackprovincia.className = "valid-feedback";
+    }
+});
+
+document.getElementById("zip").addEventListener("blur", (evento) => {
+    const input = evento.currentTarget;
+    const txtzip = input.value;
+    
+    // let o const - formas recomendadas de variables o constantes
+    const feedbackzip = document.getElementById("feedback-zip");
+
+    if(txtzip.length < 5 ) {        
+        feedbackzip.innerHTML = "Los Zip Code son de 5 numeros, por favor";
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        feedbackzip.className = "invalid-feedback";
+    } else {        
+        feedbackzip.innerHTML = "Listo";
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        feedbackzip.className = "valid-feedback";
+    }
+});
